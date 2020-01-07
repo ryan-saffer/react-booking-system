b89d04a6fa0608d0126b88a176a5dc294c3f34cc
Ryan Saffer <ryansaffer@gmail.com>
2020-01-07 18:16:03 +0800

    UI tweaks
    Improved how app looks for mobile and iPads.
    Also made it more visually obvious which fields have no value in a booking, by making the input 'filled'.

--- firebase/src/components/Bookings/BookingsNav.js
+++ firebase/src/components/Bookings/BookingsNav.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import NavigateBefore from '@material-ui/icons/NavigateBefore'
@@ -9,18 +8,12 @@ import NavigateNext from '@material-ui/icons/NavigateNext'
 const dateFormat = require('dateformat')
 
 const useStyles = makeStyles({
+    main: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
     heading: {
         textAlign: 'center'
-    },
-    beforeButton: {
-        textAlign: 'right',
-        marginTop: 'auto',
-        marginBottom: 'auto'
-    },
-    nextButton: {
-        tetAlign: 'left',
-        marginTop: 'auto',
-        marginBottom: 'auto'
     }
 })
 const DateNav = props => {
@@ -29,18 +22,12 @@ const DateNav = props => {
 
     const { onNavigateBefore, onNavigateNext, date } = props
 
-    return(
-        <Grid container spacing={3}>
-            <Grid item xs className={classes.beforeButton}>
-                <Button onClick={onNavigateBefore}><NavigateBefore /></Button>
-            </Grid>
-            <Grid item xs>
-                <Typography className={classes.heading} variant="h6">{dateFormat(date, "dddd, mmmm dS, yyyy")}</Typography>
-            </Grid>
-            <Grid item xs className={classes.nextButton}>
-                <Button onClick={onNavigateNext}><NavigateNext /></Button>
-            </Grid>
-        </Grid>
+    return (
+        <div className={classes.main}>
+            <Button onClick={onNavigateBefore}><NavigateBefore /></Button>
+            <Typography className={classes.heading} variant="h6">{dateFormat(date, "dddd, mmmm dS, yyyy")}</Typography>
+            <Button onClick={onNavigateNext}><NavigateNext /></Button>
+        </div>
     )
 }
 
