6aa694570829eae8240c0178d2e6ed2d7d3b7283
Ryan Saffer <ryansaffer@gmail.com>
2020-04-11 19:23:47 +1000

    Touchups

--- firebase/src/components/Navigation/index.js
+++ firebase/src/components/Navigation/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { compose } from 'recompose'
 
-import * as ROUTES from '../../constants/routes'
 import { CssBaseline, AppBar, Toolbar, Typography, makeStyles, Paper, Button } from '@material-ui/core'
-import { grey , red } from '@material-ui/core/colors'
+import { grey, red } from '@material-ui/core/colors'
+import Container from '@material-ui/core/Container'
+
+import * as ROUTES from '../../constants/routes'
 import { withFirebase } from '../Firebase'
 import { withAuthorization } from '../Session'
 
@@ -28,7 +30,8 @@ const useStyles = makeStyles(theme => ({
         }
     },
     signOutButton: {
-        marginTop: theme.spacing(1),
+        width: '100%',
+        marginTop: theme.spacing(4),
         backgroundColor: red[400],
         "&:hover": {
             backgroundColor: red[500]
@@ -57,15 +60,17 @@ const Navigation = props => {
                 </Toolbar>
             </AppBar>
             <div className={classes.main}>
-                <Paper className={classes.paper} onClick={() => navigateToRoute(ROUTES.BOOKINGS)}>
-                    <Typography>Party Bookings</Typography>
-                </Paper>
-                <Paper className={classes.paper} onClick={() => navigateToRoute(ROUTES.SCIENCE_CLUB_SELECT_CLASS)}>
-                    <Typography>After School Science Program</Typography>
-                </Paper>
-                <Button className={classes.signOutButton} variant="contained" onClick={firebase.doSignOut}>
-                    Sign out
-                </Button>
+                <Container component="main" maxWidth="sm">
+                    <Paper className={classes.paper} onClick={() => navigateToRoute(ROUTES.BOOKINGS)}>
+                        <Typography>Party Bookings</Typography>
+                    </Paper>
+                    <Paper className={classes.paper} onClick={() => navigateToRoute(ROUTES.SCIENCE_CLUB_SELECT_CLASS)}>
+                        <Typography>After School Science Program</Typography>
+                    </Paper>
+                    <Button className={classes.signOutButton} variant="contained" onClick={firebase.doSignOut}>
+                            Sign out
+                    </Button>
+                </Container>
             </div>
         </>
     )
