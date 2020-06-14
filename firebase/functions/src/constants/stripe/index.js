f4c97e01728383eb6909488855d7d89af9cb3fcb
Ryan Saffer <ryansaffer@gmail.com>
2020-06-14 23:00:04 +1000

    Integrated acuity and stripe using firebase functions
    Created firebase function 'sidebar', which serves html to acuity when viewing an appointment.
    This function searches for the form field 'invoice id'. If the field doesn't, appointment doesn't support invoices.
    If it does... if the value is empty, invoice is not sent, and provide option to send invoice. If it isn't empty, lookup the invoice status and display it.
    
    When hitting 'send invoice' it calls another firebase function 'sendInvoice'.
    This will search stripe for a customer with the given email, or create a new customer if not found.
    It then creates an invoice item for the science club, creates the invoice, then sends it off.

---
+++ firebase/functions/src/constants/stripe/index.js
@@ -0,0 +1,11 @@
+exports.DEV_CONFIG = {
+    PRICE_SCIENCE_CLUB: "price_1Gtj1JIhbh0YhdB7zAnYJwDN",
+    SEND_INVOICE_ENDPOINT: "https://us-central1-bookings-prod.cloudfunctions.net",
+    STRIPE_DASHBOARD: "https://dashboard.stripe.com/test"
+}
+
+exports.PROD_CONFIG = {
+    STRIPE_PRICE_SCIENCE_CLUB: "TODO",
+    SEND_INVOICE_ENDPOINT: "https://us-central1-booking-system-6435d.cloudfunctions.net",
+    STRIPE_DASHBOARD: "https://dashboard.stripe.com"
+}
