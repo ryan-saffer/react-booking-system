7cd5fa34a4bc5bee86542d12cdeb881a0a6750f1
Ryan Saffer <ryansaffer@gmail.com>
2020-07-16 15:05:02 +1000

    When customer submits form, update firestore and trigger emails
    // IN STORE ONLY //
    Added the on form submit trigger to provide the values to the new firebase function onFormSubmit.
    Function searches for the booking, and updates firestore with values, before asking apps script to send confirmation and notification emails.
    If booking is not found, go to apps script to send error email.
    
    Mobile form yet to be integrated.

--- firebase/functions/src/index.js
+++ firebase/functions/src/index.js
@@ -1,4 +1,5 @@
 import * as bookings from './bookings'
+import * as bookingsFormToSheet from './bookings/onFormSubmit'
 import * as acuity from './acuity/client'
 import * as acuityStripeIntegration from './acuity/stripe-integration'
 
@@ -6,6 +7,7 @@ export const createBooking = bookings.createBooking
 export const updateBooking = bookings.updateBooking
 export const deleteBooking = bookings.deleteBooking
 export const sendOutForms = bookings.sendOutForms
+export const onFormSubmit = bookingsFormToSheet.onFormSubmit
 
 export const acuityClient = acuity.client
 export const sidebar = acuityStripeIntegration.sidebar
