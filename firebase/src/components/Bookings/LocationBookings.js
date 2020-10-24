8ba93d5a6a3fb7b0b0a7522de4fe3e53a41f2de5
Ryan Saffer <ryansaffer@gmail.com>
2020-10-24 22:59:48 +1100

    Refactored form values constants
    Capitalised all values, and imported entire file as a namespace where used, to better separate static constants from regular local variables.
    Also used FormValues.Locations to get list of locations, instead of having the locations entered manually. Eg showing checkboxes for each location, showing bookings for each location, and validating if a location is mobile or not.

--- firebase/src/components/Bookings/LocationBookings.js
+++ firebase/src/components/Bookings/LocationBookings.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import BookingPanel from './BookingPanel'
+import * as Utilities from '../../utilities'
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -22,7 +23,7 @@ const LocationBookings = props => {
 
     return (
         <div className={classes.root}>
-            <Typography className={classes.heading} variant="h6">{location.charAt(0).toUpperCase() + location.slice(1)}</Typography>
+            <Typography className={classes.heading} variant="h6">{Utilities.capitalise(location)}</Typography>
             {filteredBookings.length > 0 ? (
                 filteredBookings.map(booking => <BookingPanel key={booking.id} onSuccess={props.onSuccess} bookingId={booking.id} booking={booking.data()} />)
             ) : (
