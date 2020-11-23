70bbf778996fdaf348b914e7aed2eb622d568a56
Ryan Saffer <ryansaffer@gmail.com>
2020-11-23 14:02:40 +1100

    Created the pubsub job to send feedback emails daily
    Also made minor changes to existing emails.

--- firebase/functions/src/index.js
+++ firebase/functions/src/index.js
@@ -7,6 +7,7 @@ export const createBooking = bookings.createBooking
 export const updateBooking = bookings.updateBooking
 export const deleteBooking = bookings.deleteBooking
 export const sendOutForms = bookings.sendOutForms
+export const sendFeedbackEmails = bookings.sendFeedbackEmails
 export const onFormSubmit = bookingsFormToSheet.onFormSubmit
 
 export const acuityClient = acuity.client
