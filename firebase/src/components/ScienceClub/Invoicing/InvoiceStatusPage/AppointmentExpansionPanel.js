d9eaacb8c81f240fed3778621887de7227d5a5ac
Ryan Saffer <ryansaffer@gmail.com>
2021-03-12 19:08:19 +1100

    Refactored sendInvoice cloud function
    Changed from `https.onRquest` to https.onCall` to be used inside the the `InvoiceStatus` accordion.
    `useInvoiceStatus` returns the set method for the status, to allow the setting of error states. Given limited scope of this hook, it felt cleaner.
    Made UI pretty.

--- firebase/src/components/ScienceClub/Invoicing/InvoiceStatusPage/AppointmentExpansionPanel.js
+++ firebase/src/components/ScienceClub/Invoicing/InvoiceStatusPage/AppointmentExpansionPanel.js
@@ -1,8 +1,13 @@
-import React from 'react'
-import { Accordion, AccordionSummary, makeStyles, Typography, Button } from '@material-ui/core'
+import React, { useContext } from 'react'
+import { Accordion, AccordionSummary, makeStyles, Typography, Button, CircularProgress, Chip,
+AccordionDetails, Table, TableBody, TableRow, TableCell } from '@material-ui/core'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { green, orange, red, blue } from '@material-ui/core/colors'
 
 import useInvoiceStatus from '../../../Hooks/UseInvoiceStatus';
+import { FirebaseContext } from '../../../Firebase'
+import * as Utilities from '../../../../utilities'
+import * as Acuity from '../../../../constants/acuity'
 
 const AppointmentExpansionPanel = props => {
     
@@ -18,11 +23,36 @@ const AppointmentExpansionPanel = props => {
         >
             <AccordionSummary expandIcon={<ExpandMoreIcon />}>
                 <div className={classes.panelSummary}>
-                    <Typography>{appointment.firstName} {appointment.lastName}</Typography>
-                    <Typography> Invoice Status: </Typography>
+                    <Typography className={classes.parentName}>{appointment.firstName} {appointment.lastName}</Typography>
                     <InvoiceStatus appointment={appointment} />
                 </div>
             </AccordionSummary>
+            <AccordionDetails>
+                <Table size="small">
+                    <TableBody>
+                        <TableRow>
+                            <TableCell variant="head">Parent Phone:</TableCell>
+                            <TableCell>{appointment.phone}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell variant="head">Parent Email:</TableCell>
+                            <TableCell>{appointment.email}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell variant="head">Child Name:</TableCell>
+                            <TableCell>{Utilities.retrieveFormAndField(appointment, Acuity.FORMS.CHILD_DETAILS, Acuity.FORM_FIELDS.CHILD_NAME)}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell variant="head">Child Age:</TableCell>
+                            <TableCell>{Utilities.retrieveFormAndField(appointment, Acuity.FORMS.CHILD_DETAILS, Acuity.FORM_FIELDS.CHILD_AGE)}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell variant="head">Child Grade:</TableCell>
+                            <TableCell>{Utilities.retrieveFormAndField(appointment, Acuity.FORMS.CHILD_DETAILS, Acuity.FORM_FIELDS.CHILD_GRADE)}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </AccordionDetails>
         </Accordion>
     )
 }
@@ -31,59 +61,103 @@ const InvoiceStatus = ({ appointment }) => {
     
     const classes = useStyles()
 
-    const { status, url } = useInvoiceStatus(appointment)
+    const firebase = useContext(FirebaseContext)
+    const [{ status, url }, setStatus] = useInvoiceStatus(appointment)
 
     const sendInvoice = (appointmentId, event) => {
         event.stopPropagation()
         console.log(appointmentId)
+        setStatus({ status: "LOADING" })
+        const childName = Utilities.retrieveFormAndField(appointment, Acuity.FORMS.CHILD_DETAILS, Acuity.FORM_FIELDS.CHILD_NAME)
+        firebase.functions.httpsCallable('sendInvoice')({
+            email: appointment.email,
+            name: `${appointment.firstName} ${appointment.lastName}`,
+            phone: appointment.phone,
+            childName: childName,
+            invoiceItem: `${childName} - ${appointment.type}`
+        })
+        .then(result => {
+            console.log('setting status', result)
+            setStatus(result.data)
+        })
+        .catch(error => {
+            console.error(error)
+            setStatus({ status: "ERROR" })
+        })
+    }
+
+    const openUrl = (url, event) => {
+        event.stopPropagation()
+        window.open(url, "_blank")
     }
 
     switch (status) {
         case "LOADING":
-            return <Typography>Loading invoice...</Typography>
+            return <CircularProgress size={24} />
         case "PAID":
             return (
-                <Typography>
-                    <a
-                        href={url}
-                        target="_blank"
-                        onClick={event => event.stopPropagation()}
-                    >
-                        Invoice paid
-                    </a>
-                </Typography>
+                <>
+                <Chip className={classes.chipPaid} label="PAID" /> 
+                <Button className={classes.viewInvoiceButton} onClick={(event) => openUrl(url, event)}>View Invoice</Button>
+                </>
             )
         case "UNPAID":
             return (
-                <Typography>
-                    <a
-                        href={url}
-                        target="_blank"
-                        onClick={event => event.stopPropagation()}
-                    >
-                        Invoice not yet paid
-                    </a>
-                </Typography>
+                <>
+                <Chip className={classes.chipUnpaid} label="NOT PAID" />
+                <Button className={classes.viewInvoiceButton} onClick={(event) => openUrl(url, event)}>View Invoice</Button>
+                </>
             )
         case "NOT_SENT":
             return (
-                <div>
-                <Typography>Invoice not yet sent!</Typography>
-                <Button onClick={(event) => sendInvoice(appointment.id, event)}>Send Invoice</Button>
-                </div>
+                <>
+                <Chip className={classes.chipNotSent} label="INVOICE NOT SENT" />
+                <Button className={classes.sendInvoiceButton} onClick={(event) => sendInvoice(appointment.id, event)}>Send Invoice</Button>
+                </>
             )
         case "UNSUPPORTED":
             return <Typography>This class does not support invoices</Typography>
         case "ERROR":
-            return <Typography>... Error while fetching invoice</Typography>
+            return <Typography className={classes.redText}>Error while fetching invoice</Typography>
     }
 }
 
+const chipWidth = 140
 const useStyles = makeStyles({
     panelSummary: {
         display: 'grid',
         width: '100%',
-        gridTemplateColumns: '1fr 1fr 1fr'
+        gridTemplateColumns: '1fr 1fr 1fr',
+        justifyItems: 'center',
+        alignItems: 'center'
+    },
+    parentName: {
+        justifySelf: 'flex-start',
+        marginLeft: '80px'
+    },
+    chipPaid: {
+        width: chipWidth,
+        background: green[500]
+    },
+    chipUnpaid: {
+        width: chipWidth,
+        background: orange[500]
+    },
+    chipNotSent: {
+        width: chipWidth,
+        background: red[500]
+    },
+    viewInvoiceButton: {
+        border: 'solid 1px black'
+    },
+    sendInvoiceButton: {
+        background: blue[400],
+        '&:hover': {
+            background: blue[600]
+        }
+    },
+    redText: {
+        color: red[500]
     }
 })
 
