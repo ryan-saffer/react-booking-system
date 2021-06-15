719edd45aaaf50b0704797c8c3140ca53950659a
Ryan Saffer <ryansaffer@gmail.com>
2021-06-16 01:16:13 +1000

    Prettied up confirmation of enrolment page
    Created Success and Error UI's, and display either depending on the result of the hook.

---
+++ firebase/src/components/ScienceClub/Enrolment/Result.tsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import { Container, Divider, Grid, makeStyles, Paper, Typography } from '@material-ui/core'
+import ErrorIcon from '@material-ui/icons/Error'
+import CheckCircleTwoTone from '@material-ui/icons/CheckCircleTwoTone'
+import { Acuity } from 'fizz-kidz/lib'
+import { retrieveFormAndField } from 'fizz-kidz/lib/acuity/utilities'
+
+type ResultType = "success" | "error"
+
+interface ResultProps {
+    label: string,
+    resultType: ResultType
+}
+
+const Result: React.FC<ResultProps> = ({ label, resultType, children }) => {
+
+    const classes = useStyles()
+
+    return (
+        <>
+            <Container maxWidth="md">
+                <Grid container justify="center">
+                    <Grid className={classes.gridItem} item sm={6}>
+                        <Typography className={classes.error} variant="h4">{label}</Typography>
+                        {resultType === "error" && <ErrorIcon className={classes.errorIcon} />}
+                        {resultType === "success" && <CheckCircleTwoTone className={classes.successIcon} />}
+                    </Grid>
+                    <Grid className={classes.gridItem} item sm={6}>
+                        <Paper className={classes.paper} elevation={3}>
+                            {children}
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+            <Divider className={classes.divider} />
+        </>
+    )
+}
+
+export const Error = () => (
+    <Result label="Whoopsies... 🤦‍♀️" resultType="error">
+        <Typography variant="h5" gutterBottom>Something went wrong</Typography>
+        <Typography variant="body1" gutterBottom >It looks like we had a problem processing your request.</Typography>
+        <Typography variant="body1" gutterBottom>Please either reply to our email, or call to let us know if you would like to continue with the term.</Typography>
+        <Typography variant="button" gutterBottom>The Fizz Kidz team</Typography>
+    </Result>
+)
+
+interface SuccessProps {
+    appointments: Acuity.Appointment[]
+}
+
+export const Success: React.FC<SuccessProps> = ({ appointments }) => {
+
+    const appointment = appointments[0]
+    const childName = retrieveFormAndField(appointment, Acuity.Constants.Forms.CHILD_DETAILS, Acuity.Constants.FormFields.CHILD_NAME)
+
+    return (
+        <Result label="Confirmed" resultType="success">
+            <Typography variant="h5" gutterBottom>Hi {appointment.firstName},</Typography>
+            <Typography variant="body1" gutterBottom >{childName}'s enrolment in the term has been confirmed.</Typography>
+            <Typography variant="body1" gutterBottom>{childName} can continue coming every week, and we will send you an invoice for the term.</Typography>
+            <Typography variant="body1" gutterBottom>We can't wait to continue the science adventure!</Typography>
+            <Typography variant="button" gutterBottom>The Fizz Kidz team</Typography>
+        </Result>
+    )
+}
+
+const useStyles = makeStyles({
+    gridItem: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: "center"
+    },
+    error: {
+        alignSelf: 'center'
+    },
+    successIcon: {
+        height: 60,
+        width: 60,
+        alignSelf: 'center',
+        color: 'green',
+        marginTop: 15,
+        marginBottom: 15
+    },
+    errorIcon: {
+        height: 60,
+        width: 60,
+        alignSelf: 'center',
+        color: 'crimson',
+        marginTop: 15,
+        marginBottom: 15
+    },
+    paper: {
+        padding: 32,
+    },
+    divider: {
+        alignSelf: 'center',
+        marginTop: 20,
+        marginBottom: 40,
+        width: '80%'
+    }
+})
