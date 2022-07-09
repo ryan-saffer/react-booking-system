177ce4d67f5547127276f9adae2306fcf57caec5
Ryan Saffer <ryansaffer@gmail.com>
2022-07-09 21:28:28 +1000

    Connected to sendgrid to send emails
    Use mustache for templating. Send holiday program confirmation using mjml and mustache and sendgrid!

--- firebase/functions/src/environment.d.ts
+++ firebase/functions/src/environment.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       FIREBASE_CONFIG: string;
+      SEND_GRID_API_KEY: string;
     }
   }
 }
---
+++ firebase/functions/src/sendgrid/EmailClient.ts
@@ -0,0 +1,66 @@
+import { Emails } from './types'
+import fs from 'fs'
+import path from 'path'
+import mjml2html from 'mjml'
+import sgMail, { MailDataRequired } from '@sendgrid/mail'
+import { EmailTemplates } from './constants'
+
+var Mustache = require('mustache')
+
+sgMail.setApiKey(process.env.SEND_GRID_API_KEY)
+
+export class MailClient {
+    async sendEmail<T extends keyof Emails>(email: T, emailInfo: Emails[T]) {
+        console.log('generating html...')
+        let html = this.generateHtml(email, emailInfo.values)
+        console.log('generated successfully!')
+        await this._sendEmail(email, emailInfo, html)
+    }
+
+    private async _sendEmail<T extends keyof Emails>(
+        email: T,
+        emailInfo: Emails[T],
+        html: string
+    ) {
+        switch (email) {
+            case 'holidayProgramConfirmation':
+                await this._sendHolidayProgramConfirmationEmail(html)
+                return
+        }
+    }
+
+    private generateHtml<T extends keyof Emails>(
+        email: T,
+        values: Emails[T]['values']
+    ): string {
+        const filename = EmailTemplates[email]
+        let mjml = fs.readFileSync(
+            path.resolve(__dirname, `./mjml/${filename}`),
+            'utf8'
+        )
+        var output = Mustache.render(mjml, values)
+        let mjmlOutput = mjml2html(output)
+        if (mjmlOutput.errors.length > 0) {
+            mjmlOutput.errors.forEach((error) => {
+                console.error(error.formattedMessage)
+            })
+            throw new Error('error converting mjml to html')
+        } else {
+            return mjmlOutput.html
+        }
+    }
+
+    private async _sendHolidayProgramConfirmationEmail(html: string) {
+        const msg: MailDataRequired = {
+            to: 'ryansaffer@gmail.com',
+            from: {
+                name: 'Fizz Kidz',
+                email: 'info@fizzkidz.com.au',
+            }, // Use the email address or domain you verified above
+            subject: 'Holiday program booking confirmation',
+            html: html,
+        }
+        console.log('sending email...')
+        await sgMail.send(msg)
+    }
+}
