63deecb83ba118c2d43f729c1ccbfbce67b017e0
Ryan Saffer <ryansaffer@gmail.com>
2022-07-23 00:52:31 +1000

    Separated prod and dev stripe webhook secret

--- firebase/functions/src/environment.d.ts
+++ firebase/functions/src/environment.d.ts
@@ -5,7 +5,8 @@ declare global {
             SEND_GRID_API_KEY: string
             ACUITY_USER_ID: string
             ACUITY_API_KEY: string
-            STRIPE_WEBHOOK_SECRET: string
+            STRIPE_WEBHOOK_SECRET_PROD: string
+            STRIPE_WEBHOOK_SECRET_DEV: string
         }
     }
 }
