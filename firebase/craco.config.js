b5938244afdac8aee106c6e9609aab6f5d4b55e1
Ryan Saffer <ryansaffer@gmail.com>
2022-08-11 22:52:19 -0500

    Added cancellation policy
    Also changed base antd font size to 16, to avoid iOS devices zooming on input focus

--- firebase/craco.config.js
+++ firebase/craco.config.js
@@ -7,7 +7,8 @@ module.exports = {
       options: {
         customizeTheme: {
           "@primary-color": "#B14592",
-          "@border-radius-base": "6px"
+          "@border-radius-base": "6px",
+          "@font-size-base": "16px"
         },
       },
     },
