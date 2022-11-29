425936049601c7508a41cc7b1a7f4aa90618d586
Ryan Saffer <ryansaffer@gmail.com>
2022-11-29 11:35:59 +1100

    limit payment intent description to 1000 chars

--- firebase/functions/src/stripe/functions/paymentIntents/createPaymentIntent.ts
+++ firebase/functions/src/stripe/functions/paymentIntents/createPaymentIntent.ts
@@ -23,13 +23,14 @@ export const createPaymentIntent = onCall<'createPaymentIntent'>(
             const key = `${it.childName.slice(0, 40 - it.dateTime.length - 3)} - ${it.dateTime}`
             programData[key] = it.amount
         })
+        const description =
+            data.description + ' - ' + data.programs.map((it) => `${it.childName} - ${it.dateTime}`).join(', ')
         const paymentIntent = await stripe.paymentIntents.create({
             customer: customerId,
             amount: data.amount,
             currency: 'aud',
             payment_method_types: ['card'],
-            description:
-                data.description + ' - ' + data.programs.map((it) => `${it.childName} - ${it.dateTime}`).join(', '),
+            description: description.slice(0, 1000), // stripe accepts at most 1000 chars - happens on many (many) bookings at once
             metadata: {
                 programType: data.programType,
                 programCount: data.programs.length,
