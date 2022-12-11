15e87acff24b031689813320b3ad0ab37fcb377b
Ryan Saffer <ryansaffer@gmail.com>
2022-12-11 15:49:50 +1100

    created external programs form
    does not yet handle submission

---
+++ firebase/src/components/Bookings/Forms/EventForm/FormFields.ts
@@ -0,0 +1,61 @@
+type FormTypes = {
+    contactName: string
+    contactNumber: string
+    contactEmail: string
+    organisation: string
+    location: string
+    date: Date | null
+    startTime: string
+    endTime: string
+    notes: string
+}
+
+export type FormFields = { [Key in keyof FormTypes]: { value: FormTypes[Key]; error: boolean; errorText: string } }
+
+export const getEmptyFormValues = (): FormFields => ({
+    contactName: {
+        value: '',
+        error: false,
+        errorText: 'Contact name cannot be empty',
+    },
+    contactNumber: {
+        value: '',
+        error: false,
+        errorText: 'Contact number cannot be empty',
+    },
+    contactEmail: {
+        value: '',
+        error: false,
+        errorText: 'Contact email cannot be empty',
+    },
+    organisation: {
+        value: '',
+        error: false,
+        errorText: 'Organisation cannot be empty',
+    },
+    location: {
+        value: '',
+        error: false,
+        errorText: 'Location cannot be empty',
+    },
+    date: {
+        value: null,
+        error: false,
+        errorText: 'Date cannot be empty',
+    },
+    startTime: {
+        value: '',
+        error: false,
+        errorText: 'Start time cannot be empty',
+    },
+    endTime: {
+        value: '',
+        error: false,
+        errorText: 'End time cannot be empty',
+    },
+    notes: {
+        value: '',
+        error: false,
+        errorText: 'Notes cannot be empty',
+    },
+})
