b6814011c372fbcf8e9a81621b35179a1c97d9b9
Ryan Saffer <ryansaffer@gmail.com>
2023-03-08 00:17:32 +1100

    Added frankfurts to reverse addition pricing mapper in form mapper

--- firebase/functions/src/bookings/utils.ts
+++ firebase/functions/src/bookings/utils.ts
@@ -1,13 +1,14 @@
 import { Additions } from 'fizz-kidz'
 
-export const AdditionsFormMap: { [key: string]: string } = {
-    "Chicken Nuggets - $35": Additions.chickenNuggets,
-    "Fairy Bread - $30": Additions.fairyBread,
-    "Fruit Platter - $45": Additions.fruitPlatter,
-    "Sandwich Platter - butter & cheese, vegemite & butter,  cheese & tomato - $35": Additions.sandwichPlatter,
-    "Vegetarian Quiches - $35": Additions.vegetarianQuiche,
-    "Watermelon Platter - $25": Additions.watermelonPlatter,
-    "Wedges - $30": Additions.wedges,
-    "Grazing Platter for Parents (Medium: 10-15 ppl) - $98": Additions.grazingPlatterMedium,
-    "Grazing Platter for Parents (Large: 15-25 ppl) - $148": Additions.grazingPlatterLarge
+export const AdditionsFormMap: { [key: string]: Additions } = {
+    'Chicken Nuggets - $35': Additions.chickenNuggets,
+    'Fairy Bread - $30': Additions.fairyBread,
+    'Fruit Platter - $45': Additions.fruitPlatter,
+    'Frankfurts - $25': Additions.frankfurts,
+    'Sandwich Platter - butter & cheese, vegemite & butter,  cheese & tomato - $35': Additions.sandwichPlatter,
+    'Vegetarian Quiches - $35': Additions.vegetarianQuiche,
+    'Watermelon Platter - $25': Additions.watermelonPlatter,
+    'Wedges - $30': Additions.wedges,
+    'Grazing Platter for Parents (Medium: 10-15 ppl) - $98': Additions.grazingPlatterMedium,
+    'Grazing Platter for Parents (Large: 15-25 ppl) - $148': Additions.grazingPlatterLarge,
 }
