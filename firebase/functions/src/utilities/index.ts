c4ab3945add6148285dee32a0545d3a744142a89
Ryan Saffer <ryansaffer@gmail.com>
2023-05-26 00:50:42 +1000

    created an exponential backoff utility function this is useful for calendar, where rate limiting exists when creating multiple events at once. Exponential backoff fixes this.

--- firebase/functions/src/utilities/index.ts
+++ firebase/functions/src/utilities/index.ts
@@ -18,3 +18,38 @@ export function onRequest<T extends keyof FirebaseFunctions>(
 ) {
     return functions.region('australia-southeast1').https.onRequest(fn)
 }
+
+/**
+ * Retry a function with exponential backoff if it fails with expected error codes. Will retry 3 times before failing for good.
+ *
+ * @param fn the function you want to run
+ * @param backoffCodes the error codes you want to retry for. Any error codes faced that are not included here will not be retried.
+ * @param retryCount keeps track of the number of times the function has been attempted.
+ */
+export function withExponentialBackoff<T extends () => any>(
+    fn: T,
+    backoffCodes: number[],
+    retryCount = 1
+): Promise<ReturnType<T>> {
+    return new Promise((resolve, reject) => {
+        const runFunction = async () => {
+            try {
+                const result = await fn()
+                resolve(result)
+            } catch (err: any) {
+                if (backoffCodes.includes(err.code)) {
+                    if (retryCount <= 3) {
+                        console.log(`Error code ${err.code} found. Running again, with retryCount of ${retryCount + 1}`)
+                        setTimeout(runFunction, Math.pow(2, retryCount + 1))
+                    } else {
+                        reject(err)
+                    }
+                } else {
+                    reject(err)
+                }
+            }
+        }
+
+        runFunction()
+    })
+}
