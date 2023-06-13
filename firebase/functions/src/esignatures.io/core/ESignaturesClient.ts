02d620b02f4b9eec9553bb3728303e6e7df17887
Ryan Saffer <ryansaffer@gmail.com>
2023-06-13 16:04:59 +1000

    created esignatures client and contract template

---
+++ firebase/functions/src/esignatures.io/core/ESignaturesClient.ts
@@ -0,0 +1,42 @@
+import { env } from '../../init'
+
+const FACILITATOR_CONTRACT_TEMPLATE_ID = 'ae77f4ae-8a4d-438a-82b1-b27d49b45ea9'
+
+export class ESignatureClient {
+    async sendContract({
+        email,
+        mobile,
+        templateVariables,
+    }: {
+        email: string
+        mobile: string
+        templateVariables: {
+            name: string
+            position: string
+            managerName: string
+            managerPosition: string
+            address: string
+            commencementDate: string
+            wage: string
+            sendersName: string
+            sendersPosition: string
+        }
+    }) {
+        const response = await fetch(`https://esignatures.io/api/contracts?token=${process.env.ESIGNATURES_SECRET}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                template_id: FACILITATOR_CONTRACT_TEMPLATE_ID,
+                test: env === 'dev' ? 'yes' : 'no',
+                signers: [{ name: templateVariables.name, email, mobile }],
+                placeholder_fields: Object.keys(templateVariables).map((it) => ({
+                    api_key: it,
+                    value: templateVariables[it as keyof typeof templateVariables],
+                })),
+            }),
+        })
+        return response.json()
+    }
+}
