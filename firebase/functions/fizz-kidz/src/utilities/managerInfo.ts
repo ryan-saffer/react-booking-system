ab43cbc237b0cb6a8ac146e91a591c639097535a
Ryan Saffer <ryansaffer@gmail.com>
2023-09-06 11:33:58 +1000

    use balwyn@fizzkidz.com.au for balwyn communications

--- firebase/functions/fizz-kidz/src/utilities/managerInfo.ts
+++ firebase/functions/fizz-kidz/src/utilities/managerInfo.ts
@@ -3,7 +3,7 @@ import { Locations } from '../partyBookings/Locations'
 export function getManager(location: Locations) {
     switch (location) {
         case Locations.BALWYN:
-            return { name: 'Bonnie', email: 'bonnie@fizzkidz.com.au', mobile: '0413 268 795' }
+            return { name: 'Bonnie', email: 'balwyn@fizzkidz.com.au', mobile: '0413 268 795' }
         case Locations.ESSENDON:
             return { name: 'Ali', email: 'essendon@fizzkidz.com.au', mobile: '0438 328 327' }
         case Locations.MOBILE:
