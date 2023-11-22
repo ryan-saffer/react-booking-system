f297f5c055b692e1029ca2dca0706f1af3996774
Ryan Saffer <ryansaffer@gmail.com>
2023-11-22 15:09:35 +1100

    fixed firebase deploy build scripts to work with new project structure

--- client/predeploy.sh
+++ client/predeploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [[ $GCLOUD_PROJECT == 'booking-system-6435d' ]]; then
-    npm run build:dev
+    cd client && npm run build:dev
     exit 0
 elif [[ $GCLOUD_PROJECT == 'bookings-prod' ]]; then
-    npm run build:prod
+    cd client && npm run build:prod
     exit 0
 else
     echo "firebase project $GCLOUD_PROJECT not recognized"
