de4067224bf97c45681a88c9d6a5f40307e0e2f3
Ryan Saffer <ryansaffer@gmail.com>
2023-12-13 20:40:16 +1100

    updated manager info

--- server/fizz-kidz/src/utilities/managerInfo.ts
+++ server/fizz-kidz/src/utilities/managerInfo.ts
@@ -3,13 +3,13 @@ import { Location } from '../partyBookings/Locations'
 export function getManager(location: Location) {
     switch (location) {
         case Location.BALWYN:
-            return { name: 'Bonnie', email: 'balwyn@fizzkidz.com.au', mobile: '0413 268 795' }
+            return { name: 'Ebony', email: 'balwyn@fizzkidz.com.au', mobile: '0413 268 795' }
         case Location.ESSENDON:
             return { name: 'Ali', email: 'essendon@fizzkidz.com.au', mobile: '0438 328 327' }
         case Location.CHELTENHAM:
             return { name: 'Bronwyn', email: 'cheltenham@fizzkidz.com.au', mobile: '0435 906 395' }
         case Location.MALVERN:
-            return { name: 'Emily', email: 'malvern@fizzkidz.com.au', mobile: '0413 268 795' }
+            return { name: 'Emily', email: 'malvern@fizzkidz.com.au', mobile: '0490 501 376' }
         default: {
             const exhaustiveCheck: never = location
             throw new Error(`Unknown location: ${exhaustiveCheck}`)
