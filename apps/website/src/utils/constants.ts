c0884bb5ed142a9935a0dd9ab32237be55249de5
Ryan Saffer <ryansaffer@gmail.com>
2024-07-26 20:52:51 +1000

    updated website form endpoint

--- apps/website/src/utils/constants.ts
+++ apps/website/src/utils/constants.ts
@@ -1,2 +1,2 @@
 export const FORM_WEBHOOK =
-  "https://3488-116-255-54-0.ngrok-free.app/booking-system-6435d/australia-southeast1/contactForm7WebhookV2";
+  "https://australia-southeast1-bookings-prod.cloudfunctions.net/websiteFormsWebhook";
