f2122ea4311a4f406a87f344ea49de8e225c8ce2
Ryan Saffer <ryansaffer@gmail.com>
2024-08-07 17:42:11 +1000

    added robots.txt

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.fizzkidz.com.au",
+  site: "https://fizzkidz.com.au",
   output: "hybrid",
   adapter: netlify(),
   integrations: [
