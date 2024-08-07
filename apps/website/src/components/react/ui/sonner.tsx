eb3ccf76a25e7a14d5bb6faeef2955a7e7d41b79
Ryan Saffer <ryansaffer@gmail.com>
2024-08-07 14:51:05 +1000

    upgrade to latest astro version, fix warnings

--- apps/website/src/components/react/ui/sonner.tsx
+++ apps/website/src/components/react/ui/sonner.tsx
@@ -1,11 +1,8 @@
 import { Toaster as Sonner } from "sonner";
-import { useTheme } from "next-themes";
 
 type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
-
   return (
     <Sonner
       theme="light"
