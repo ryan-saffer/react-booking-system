a8002fc8de5b14cb6fecb90de0d69aba58e814c6
Ryan Saffer <ryansaffer@gmail.com>
2024-09-01 13:16:00 +1000

    updated manager info"

--- server/fizz-kidz/src/utilities/managerInfo.ts
+++ server/fizz-kidz/src/utilities/managerInfo.ts
@@ -5,9 +5,9 @@ export function getManager(location: Location) {
         case Location.BALWYN:
             return { name: 'Bronwyn', email: 'balwyn@fizzkidz.com.au', mobile: '0435 906 395' }
         case Location.ESSENDON:
-            return { name: 'Ali', email: 'essendon@fizzkidz.com.au', mobile: '0438 328 327' }
+            return { name: 'Bonnie', email: 'essendon@fizzkidz.com.au', mobile: '0431 379 953' }
         case Location.CHELTENHAM:
-            return { name: 'Bronwyn', email: 'cheltenham@fizzkidz.com.au', mobile: '0435 906 395' }
+            return { name: 'Ben', email: 'cheltenham@fizzkidz.com.au', mobile: '0490 450 282' }
         case Location.MALVERN:
             return { name: 'Emily', email: 'malvern@fizzkidz.com.au', mobile: '0413 268 795' }
         default: {
