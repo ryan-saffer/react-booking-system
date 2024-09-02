21d6be2cc3d0367282f7281ff864b4c0ede41828
Ryan Saffer <ryansaffer@gmail.com>
2024-09-02 22:40:40 +1000

    make lami essendon manager for now

--- server/fizz-kidz/src/utilities/managerInfo.ts
+++ server/fizz-kidz/src/utilities/managerInfo.ts
@@ -5,7 +5,7 @@ export function getManager(location: Location) {
         case Location.BALWYN:
             return { name: 'Bronwyn', email: 'balwyn@fizzkidz.com.au', mobile: '0435 906 395' }
         case Location.ESSENDON:
-            return { name: 'Bonnie', email: 'essendon@fizzkidz.com.au', mobile: '0431 379 953' }
+            return { name: 'Lami', email: 'essendon@fizzkidz.com.au', mobile: '0431 379 953' }
         case Location.CHELTENHAM:
             return { name: 'Ben', email: 'cheltenham@fizzkidz.com.au', mobile: '0490 450 282' }
         case Location.MALVERN:
