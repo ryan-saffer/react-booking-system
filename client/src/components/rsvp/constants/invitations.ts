ccc5a7294efa56f47da9850f7d9fe82e6fa3b48c
Ryan Saffer <ryansaffer@gmail.com>
2024-10-06 13:07:02 +1100

    invitation creation flow

---
+++ client/src/components/rsvp/constants/invitations.ts
@@ -0,0 +1,40 @@
+import { InvitationOption } from 'fizz-kidz'
+
+export const INVITATIONS: { name: InvitationOption; src: string }[] = [
+    {
+        name: 'Freckles',
+        src: '/invitations/Invitation+Envelope-Freckles.png',
+    },
+    {
+        name: 'Stripes',
+        src: '/invitations/Invitation+Envelope-Stripes.png',
+    },
+    {
+        name: 'Dots',
+        src: '/invitations/Invitation+Envelope-Dots.png',
+    },
+    {
+        name: 'Glitz & Glam',
+        src: '/invitations/Invitation+Envelope-Glitz.png',
+    },
+    {
+        name: 'Swiftie',
+        src: '/invitations/Invitation+Envelope-Swift.png',
+    },
+    {
+        name: 'Bubbling Fun',
+        src: '/invitations/Invitation+Envelope-Bubbling.png',
+    },
+    {
+        name: 'Bubbling Blue Fun',
+        src: '/invitations/Invitation+Envelope-Bubbling-Blue.png',
+    },
+    {
+        name: 'Slime Time',
+        src: '/invitations/Invitation+Envelope-Slime.png',
+    },
+    {
+        name: 'Tie Dye',
+        src: '/invitations/Invitation+Envelope-Tye-Dye.png',
+    },
+]
---
+++ client/src/components/rsvp/login-dialog.tsx
@@ -0,0 +1,168 @@
+import { Info, Loader2 } from 'lucide-react'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import useFirebase from '@components/Hooks/context/UseFirebase'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button } from '@ui-components/button'
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from '@ui-components/dialog'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@ui-components/form'
+import { Input } from '@ui-components/input'
+import { Separator } from '@ui-components/separator'
+
+const formSchema = z.object({
+    email: z.string().min(1, { message: 'Email cannot be empty.' }).email(),
+    password: z.string().min(8, { message: 'Password must be at least 8 characters long.' }),
+})
+
+export function LoginDialog({ open }: { open: boolean }) {
+    const firebase = useFirebase()
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+    })
+
+    const [loading, setLoading] = useState(false)
+
+    const signInWithGoogle = async () => {
+        try {
+            await firebase.linkWithGoogle()
+            console.log('link succesful')
+        } catch (err: any) {
+            console.error({ err })
+            if (err.code === 'auth/credential-already-in-use') {
+                try {
+                    // TODO: User already exists, so move their invitation data to the existing user
+                    // 1. look up the existing users uid using the err.customData.email
+                    // 2. move their invitation data to this user
+                    // 3. sign in using this credential (err.credential)
+
+                    await firebase.signInWithCredential(err.credential)
+                    return
+                } catch (error: any) {
+                    toast.error(error?.message)
+                    return
+                }
+            }
+            toast.error(err?.message)
+        }
+    }
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log({ values })
+        try {
+            setLoading(true)
+            await firebase.doCreateUserWithEmailAndPassword(values.email, values.password)
+        } catch (err: any) {
+            if (err.code == 'auth/weak-password') {
+                toast.error('Password is too weak.')
+            } else if (err.code === 'auth/email-already-in-use') {
+                toast.error(
+                    <div className="flex items-center gap-4 font-medium">
+                        <Info className="h-10 w-10" />
+                        <span>
+                            An account with this email already exists. If you have forgotten your password, you can{' '}
+                            <Link
+                                to="/reset-password"
+                                state={{
+                                    afterLoginUrl: '/invitations-v2/choose',
+                                }}
+                                className="font-bold text-destructive"
+                            >
+                                reset it here.
+                            </Link>
+                        </span>
+                    </div>,
+                    { duration: 10000, closeButton: true }
+                )
+            } else {
+                toast.error('Invalid email address and password.')
+            }
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    return (
+        <Dialog open={open}>
+            <DialogContent className="twp sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Please login to continue</DialogTitle>
+                    <DialogDescription>
+                        In order to share your invitation, and track RSVP's, you will need to create an account. This is
+                        to protect you and your guests' information.
+                        <br />
+                        <br />
+                        It will just take 10 seconds, we promise!
+                    </DialogDescription>
+                </DialogHeader>
+                <Button
+                    className="mt-4 flex h-8 w-full gap-4 border bg-white text-gray-500 shadow-sm hover:bg-gray-100"
+                    onClick={signInWithGoogle}
+                >
+                    <img className="w-4" src="https://img.clerk.com/static/google.svg?width=160" />
+                    Continue with Google
+                </Button>
+                <div className="flex w-full items-center gap-4">
+                    <Separator className="shrink" />
+                    <p className="text-[13px] font-light text-[#747686]">or</p>
+                    <Separator className="shrink" />
+                </div>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="grid w-full max-w-sm items-center gap-1.5">
+                        <FormField
+                            control={form.control}
+                            name="email"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel className="text-[13px]">Email address</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} className="h-8" />
+                                    </FormControl>
+                                    <FormMessage className="text-[13px] italic" />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel className="text-[13px]">Password</FormLabel>
+                                    <FormControl>
+                                        <Input {...field} className="h-8" type="password" />
+                                    </FormControl>
+                                    <FormMessage className="text-[13px] italic" />
+                                </FormItem>
+                            )}
+                        />
+                        <Button className="mt-4 h-8 bg-[#B14594] hover:bg-[#b0288c]" type="submit">
+                            {loading ? <Loader2 className="h-4 w-4 animate-spin" /> : 'Create account'}
+                        </Button>
+                    </form>
+                </Form>
+                <div className="flex h-12 flex-col  items-center justify-center gap-2">
+                    <p className="text-center text-[13px] text-[#747686]">
+                        Already have an account?{' '}
+                        <span className="font-medium text-[#B14594] hover:underline">
+                            <Link
+                                to="/sign-in"
+                                state={{
+                                    afterLoginUrl: '/invitations-v2/choose',
+                                }}
+                            >
+                                Sign in
+                            </Link>
+                        </span>
+                    </p>
+                </div>
+            </DialogContent>
+        </Dialog>
+    )
+}
