dbc45e14414dc58d009a69665f3534b14479c50b
Ryan Saffer <ryansaffer@gmail.com>
2024-10-07 16:14:31 +1100

    use file structure for build output

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -23,6 +23,9 @@ export default defineConfig({
     }),
   ],
   trailingSlash: "never",
+  build: {
+    format: "file",
+  },
   redirects: {
     "in-store-parties": "/birthday-parties",
     "glam-parties": "/birthday-parties/glam-parties",
