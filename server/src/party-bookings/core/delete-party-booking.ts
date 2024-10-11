0e2bb8b5ade9ed70e47382e75552f2834ce947ab
Ryan Saffer <ryansaffer@gmail.com>
2024-10-11 16:25:16 +1100

    store rsvp in db
    also ensure when deleting bookings, this collection is cleaned up, with a warning during deletion.
    made result screen after rsvping

--- server/src/party-bookings/core/delete-party-booking.ts
+++ server/src/party-bookings/core/delete-party-booking.ts
@@ -2,6 +2,7 @@ import { DatabaseClient } from '../../firebase/DatabaseClient'
 import { CalendarClient } from '../../google/CalendarClient'
 import { throwTrpcError } from '../../utilities'
 import type { DeletePartyBooking } from '../functions/trpc/trpc.parties'
+import { deleteInvitationV2 } from './delete-invitation-v2'
 
 export async function deletePartyBooking({ eventId, type, location, bookingId }: DeletePartyBooking) {
     const calendarClient = await CalendarClient.getInstance()
@@ -11,5 +12,12 @@ export async function deletePartyBooking({ eventId, type, location, bookingId }:
     } catch (err) {
         throwTrpcError('INTERNAL_SERVER_ERROR', `error deleting party booking with id: '${bookingId}'`, err)
     }
+
+    // if this party has an invitation, delete it
+    const booking = await DatabaseClient.getPartyBooking(bookingId)
+    if (booking.invitationId) {
+        await deleteInvitationV2(booking.invitationId)
+    }
+
     await DatabaseClient.deletePartyBooking(bookingId)
 }
