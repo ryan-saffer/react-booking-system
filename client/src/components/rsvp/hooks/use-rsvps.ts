319afb90bd87a76805cd7bfee1b7930a25a5aafe
Ryan Saffer <ryansaffer@gmail.com>
2024-10-14 23:28:36 +1100

    created table to view rsvps, and update an rsvp

--- client/src/components/rsvp/hooks/use-rsvps.ts
+++ client/src/components/rsvp/hooks/use-rsvps.ts
@@ -1,4 +1,4 @@
-import { InvitationsV2, Rsvp } from 'fizz-kidz'
+import { InvitationsV2, Rsvp, Service } from 'fizz-kidz'
 import { useEffect, useState } from 'react'
 
 import useFirebase from '@components/Hooks/context/UseFirebase'
@@ -6,11 +6,33 @@ import useFirebase from '@components/Hooks/context/UseFirebase'
 export function useRsvps(invitation: InvitationsV2.Invitation) {
     const firebase = useFirebase()
 
-    const [rsvps, setRsvps] = useState<Rsvp[]>([])
+    const [rsvps, setRsvps] = useState<
+        Service<{
+            rsvps: Rsvp[]
+            attendingCount: number
+            notAttendingCount: number
+            updateRsvp: (id: string, rsvp: Rsvp['rsvp']) => Promise<void>
+        }>
+    >({ status: 'loading' })
+
+    function updateRsvp(id: string, rsvp: Rsvp['rsvp']) {
+        return firebase.db.doc(`bookings/${invitation.bookingId}/rsvps/${id}`).update({ rsvp })
+    }
 
     useEffect(() => {
         const unsub = firebase.db.collection(`bookings/${invitation.bookingId}/rsvps`).onSnapshot((snap) => {
-            setRsvps(snap.docs.map((doc) => doc.data() as Rsvp))
+            const rsvps = snap.docs.map((doc) => doc.data() as Rsvp)
+            const attendingCount = rsvps.filter((it) => it.rsvp === 'attending').length
+            const notAttendingCount = rsvps.filter((it) => it.rsvp === 'not-attending').length
+            setRsvps({
+                status: 'loaded',
+                result: {
+                    rsvps,
+                    attendingCount,
+                    notAttendingCount,
+                    updateRsvp,
+                },
+            })
         })
 
         return () => unsub()
---
+++ client/src/components/rsvp/manage-rsvps/manage-rsvps.tsx
@@ -0,0 +1,138 @@
+import { InvitationsV2 } from 'fizz-kidz'
+
+import { useRsvps } from '../hooks/use-rsvps'
+import { UseRsvpProps, useRsvpTable } from '../hooks/use-rsvp-table'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@ui-components/table'
+import { flexRender } from '@tanstack/react-table'
+import { Skeleton } from '@ui-components/skeleton'
+import { Fragment } from 'react'
+import { cn } from '@utils/tailwind'
+
+const emptyState: UseRsvpProps = {
+    rsvps: [],
+    updateRsvp: () => {},
+}
+
+export function ManageRsvps({ invitation }: { invitation: InvitationsV2.Invitation }) {
+    const rsvps = useRsvps(invitation)
+
+    const table = useRsvpTable(
+        rsvps.status === 'loaded'
+            ? {
+                  rsvps: rsvps.result.rsvps,
+                  updateRsvp: rsvps.result.updateRsvp,
+              }
+            : emptyState
+    )
+
+    return (
+        <div className="p-4">
+            {rsvps.status === 'loading' && <Skeleton className="h-[200px]" />}
+            {rsvps.status === 'loaded' && (
+                <div className="m-auto w-full max-w-5xl">
+                    <p className="my-2 text-center text-2xl font-bold">RSVP Status</p>
+                    <p className="mb-6 mt-2 text-center text-xl">
+                        <span className="font-extrabold">{rsvps.result.attendingCount}</span> Attending,{' '}
+                        <span className="font-extrabold">{rsvps.result.notAttendingCount}</span> Unable to attend
+                    </p>
+                    <div className="rounded-md border">
+                        <Table>
+                            <TableHeader>
+                                {table.getHeaderGroups().map((headerGroup) => (
+                                    <TableRow key={headerGroup.id}>
+                                        {headerGroup.headers.map((header) => {
+                                            return (
+                                                <TableHead
+                                                    key={header.id}
+                                                    className={cn(
+                                                        'text-xs sm:text-sm',
+                                                        header.column.columnDef.meta?.headerClassName
+                                                    )}
+                                                >
+                                                    {header.isPlaceholder
+                                                        ? null
+                                                        : flexRender(
+                                                              header.column.columnDef.header,
+                                                              header.getContext()
+                                                          )}
+                                                </TableHead>
+                                            )
+                                        })}
+                                    </TableRow>
+                                ))}
+                            </TableHeader>
+                            <TableBody>
+                                {table.getRowModel().rows?.length ? (
+                                    table.getRowModel().rows.map((row) => (
+                                        <Fragment key={row.id}>
+                                            <TableRow
+                                                onClick={() => row.toggleExpanded(!row.getIsExpanded())}
+                                                className="cursor-pointer"
+                                            >
+                                                {row.getVisibleCells().map((cell) => (
+                                                    <TableCell
+                                                        key={cell.id}
+                                                        className={cn('p-2', cell.column.columnDef.meta?.cellClassName)}
+                                                    >
+                                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                                    </TableCell>
+                                                ))}
+                                            </TableRow>
+                                            {row.getIsExpanded() && (
+                                                <>
+                                                    {row.original.hasAllergies && row.original.allergies && (
+                                                        <ExpandableContent
+                                                            label="Allergies"
+                                                            value={row.original.allergies}
+                                                        />
+                                                    )}
+                                                    <ExpandableContent
+                                                        label="Parent Name:"
+                                                        value={row.original.parentName}
+                                                    />
+                                                    <ExpandableContent
+                                                        label="Parent Phone:"
+                                                        value={row.original.parentMobile}
+                                                    />
+                                                    <ExpandableContent
+                                                        label="Parent Email:"
+                                                        value={row.original.parentEmail}
+                                                    />
+                                                    {row.original.message && (
+                                                        <ExpandableContent
+                                                            label="Message:"
+                                                            value={row.original.message}
+                                                        />
+                                                    )}
+                                                </>
+                                            )}
+                                        </Fragment>
+                                    ))
+                                ) : (
+                                    <TableRow>
+                                        <TableCell colSpan={table.getAllColumns().length} className="h-24 text-center">
+                                            No RSVP's.
+                                        </TableCell>
+                                    </TableRow>
+                                )}
+                            </TableBody>
+                        </Table>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+function ExpandableContent({ label, value }: { label: string; value: string }) {
+    return (
+        <TableRow>
+            <TableCell className="p-2" colSpan={2}>
+                {label}
+            </TableCell>
+            <TableCell className="p-2" colSpan={2}>
+                {value}
+            </TableCell>
+        </TableRow>
+    )
+}
