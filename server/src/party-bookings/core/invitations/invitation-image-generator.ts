124f2be3b395c79e71afecef370d9be256df0ede
Ryan Saffer <ryansaffer@gmail.com>
2024-10-21 17:32:57 +1100

    dynamically create the correct invitation
    made carousel selector nicer

--- server/src/party-bookings/core/invitations/invitation-image-generator.ts
+++ server/src/party-bookings/core/invitations/invitation-image-generator.ts
@@ -1,13 +1,15 @@
 import { createCanvas, loadImage, PNGStream, registerFont } from 'canvas'
-import { InvitationOption, InvitationsV2, ObjectKeys, WithoutId, WithoutUid } from 'fizz-kidz'
+import { getApplicationDomain, InvitationOption, InvitationsV2, ObjectKeys, WithoutUid } from 'fizz-kidz'
 import fs from 'fs'
 import { DateTime } from 'luxon'
 import path from 'path'
+import QRCode from 'qrcode'
+import { env } from '../../../init'
 
 export class InvitationImageGenerator {
-    #invitation: WithoutId<WithoutUid<InvitationsV2.Invitation>>
+    #invitation: WithoutUid<InvitationsV2.Invitation>
 
-    constructor(invitation: WithoutId<WithoutUid<InvitationsV2.Invitation>>) {
+    constructor(invitation: WithoutUid<InvitationsV2.Invitation>) {
         this.#invitation = invitation
     }
 
@@ -17,26 +19,39 @@ export class InvitationImageGenerator {
      */
     async generatePng(out: string) {
         registerFont(path.resolve(__dirname, './party-bookings/invitations/fonts/lilita-one.ttf'), { family: 'lilita' })
-        const image = await loadImage(path.resolve(__dirname, './party-bookings/invitations/invitation-test.png'))
+        const image = await loadImage(
+            path.resolve(
+                __dirname,
+                `./party-bookings/invitations/${InvitationInfo[this.#invitation.invitation].filename}`
+            )
+        )
 
         const canvas = createCanvas(image.width, image.height)
         const ctx = canvas.getContext('2d')
 
         ctx.drawImage(image, 0, 0, image.width, image.height)
 
-        ObjectKeys(INVITATION_TEXT_INFO[this.#invitation.invitation]).map((key) => {
+        ObjectKeys(InvitationInfo[this.#invitation.invitation].textInfo).map((key) => {
             const {
                 font,
                 textAlign,
                 fillStyle,
                 coords: { x, y },
-            } = INVITATION_TEXT_INFO[this.#invitation.invitation][key]
+            } = InvitationInfo[this.#invitation.invitation].textInfo[key]
 
             ctx.font = font
             ctx.fillStyle = fillStyle
             ctx.textAlign = textAlign
             ctx.fillText(this.#getContent(key), x, y)
         })
+
+        const qrCodeBuffer = await QRCode.toBuffer(
+            `${getApplicationDomain(env)}/invitation/v2/${this.#invitation.id}`,
+            { width: 300 }
+        )
+        const qrCodeImage = await loadImage(qrCodeBuffer)
+        console.log({ width: image.width, height: image.height })
+        ctx.drawImage(qrCodeImage, image.width - qrCodeImage.width, image.height - qrCodeImage.height)
 
         const stream = canvas.createPNGStream()
         const outputStream = fs.createWriteStream(out)
@@ -86,131 +101,158 @@ type InvitationCoordinates = {
     date: TextInfo
 }
 
-const INVITATION_TEXT_INFO: Record<InvitationOption, InvitationCoordinates> = {
+const InvitationInfo: Record<InvitationOption, { filename: string; textInfo: InvitationCoordinates }> = {
     Freckles: {
-        childName: {
-            font: '120px lilita',
-            textAlign: 'center',
-            fillStyle: '#ABC954',
-            coords: { x: 740, y: 1060 },
-        },
-        date: {
-            font: 'bold 40px Open Sans Condensed Light',
-            fillStyle: 'black',
-            textAlign: 'left',
-            coords: { x: 482, y: 1345 },
+        filename: 'freckles.png',
+        textInfo: {
+            childName: {
+                font: '120px lilita',
+                textAlign: 'center',
+                fillStyle: '#ABC954',
+                coords: { x: 740, y: 1060 },
+            },
+            date: {
+                font: 'bold 40px Open Sans Condensed Light',
+                fillStyle: 'black',
+                textAlign: 'left',
+                coords: { x: 482, y: 1345 },
+            },
         },
     },
     Stripes: {
-        childName: {
-            font: '120px lilita',
-            textAlign: 'center',
-            fillStyle: '#ABC954',
-            coords: { x: 740, y: 1060 },
-        },
-        date: {
-            font: 'bold 40px Open Sans Condensed Light',
-            fillStyle: 'black',
-            textAlign: 'left',
-            coords: { x: 482, y: 1345 },
+        filename: 'stripes.png',
+        textInfo: {
+            childName: {
+                font: '120px lilita',
+                textAlign: 'center',
+                fillStyle: '#ABC954',
+                coords: { x: 740, y: 1060 },
+            },
+            date: {
+                font: 'bold 40px Open Sans Condensed Light',
+                fillStyle: 'black',
+                textAlign: 'left',
+                coords: { x: 482, y: 1345 },
+            },
         },
     },
     Dots: {
-        childName: {
-            font: '120px lilita',
-            textAlign: 'center',
-            fillStyle: '#ABC954',
-            coords: { x: 740, y: 1060 },
-        },
-        date: {
-            font: 'bold 40px Open Sans Condensed Light',
-            fillStyle: 'black',
-            textAlign: 'left',
-            coords: { x: 482, y: 1345 },
+        filename: 'dots.png',
+        textInfo: {
+            childName: {
+                font: '120px lilita',
+                textAlign: 'center',
+                fillStyle: '#ABC954',
+                coords: { x: 740, y: 1060 },
+            },
+            date: {
+                font: 'bold 40px Open Sans Condensed Light',
+                fillStyle: 'black',
+                textAlign: 'left',
+                coords: { x: 482, y: 1345 },
+            },
         },
     },
     'Glitz & Glam': {
-        childName: {
-            font: '120px lilita',
-            textAlign: 'center',
-            fillStyle: '#ABC954',
-            coords: { x: 740, y: 1060 },
-        },
-        date: {
-            font: 'bold 40px Open Sans Condensed Light',
-            fillStyle: 'black',
-            textAlign: 'left',
-            coords: { x: 482, y: 1345 },
+        filename: 'glitz.png',
+        textInfo: {
+            childName: {
+                font: '120px lilita',
+                textAlign: 'center',
+                fillStyle: '#ABC954',
+                coords: { x: 740, y: 1060 },
+            },
+            date: {
+                font: 'bold 40px Open Sans Condensed Light',
+                fillStyle: 'black',
+                textAlign: 'left',
+                coords: { x: 482, y: 1345 },
+            },
         },
     },
     'Bubbling Fun': {
-        childName: {
-            font: '120px lilita',
-            textAlign: 'center',
-            fillStyle: '#ABC954',
-            coords: { x: 740, y: 1060 },
-        },
-        date: {
-            font: 'bold 40px Open Sans Condensed Light',
-            fillStyle: 'black',
-            textAlign: 'left',
-            coords: { x: 482, y: 1345 },
+        filename: 'bubbling.png',
+        textInfo: {
+            childName: {
+                font: '120px lilita',
+                textAlign: 'center',
+                fillStyle: '#ABC954',
+                coords: { x: 740, y: 1060 },
+            },
+            date: {
+                font: 'bold 40px Open Sans Condensed Light',
+                fillStyle: 'black',
+                textAlign: 'left',
+                coords: { x: 482, y: 1345 },
+            },
         },
     },
     'Bubbling Blue Fun': {
-        childName: {
-            font: '120px lilita',
-            textAlign: 'center',
-            fillStyle: '#ABC954',
-            coords: { x: 740, y: 1060 },
-        },
-        date: {
-            font: 'bold 40px Open Sans Condensed Light',
-            fillStyle: 'black',
-            textAlign: 'left',
-            coords: { x: 482, y: 1345 },
+        filename: 'bubbling-blue.png',
+        textInfo: {
+            childName: {
+                font: '120px lilita',
+                textAlign: 'center',
+                fillStyle: '#ABC954',
+                coords: { x: 740, y: 1060 },
+            },
+            date: {
+                font: 'bold 40px Open Sans Condensed Light',
+                fillStyle: 'black',
+                textAlign: 'left',
+                coords: { x: 482, y: 1345 },
+            },
         },
     },
     'Slime Time': {
-        childName: {
-            font: '120px lilita',
-            textAlign: 'center',
-            fillStyle: '#ABC954',
-            coords: { x: 740, y: 1060 },
-        },
-        date: {
-            font: 'bold 40px Open Sans Condensed Light',
-            fillStyle: 'black',
-            textAlign: 'left',
-            coords: { x: 482, y: 1345 },
-        },
-    },
-    'Tie Dye': {
-        childName: {
-            font: '120px lilita',
-            textAlign: 'center',
-            fillStyle: '#ABC954',
-            coords: { x: 740, y: 1060 },
-        },
-        date: {
-            font: 'bold 40px Open Sans Condensed Light',
-            fillStyle: 'black',
-            textAlign: 'left',
-            coords: { x: 482, y: 1345 },
+        filename: 'slime.png',
+        textInfo: {
+            childName: {
+                font: '120px lilita',
+                textAlign: 'center',
+                fillStyle: '#ABC954',
+                coords: { x: 740, y: 1060 },
+            },
+            date: {
+                font: 'bold 40px Open Sans Condensed Light',
+                fillStyle: 'black',
+                textAlign: 'left',
+                coords: { x: 482, y: 1345 },
+            },
         },
     },
     Swiftie: {
-        childName: {
-            font: '120px lilita',
-            textAlign: 'center',
-            fillStyle: '#ABC954',
-            coords: { x: 740, y: 1060 },
+        filename: 'swift.png',
+        textInfo: {
+            childName: {
+                font: '120px lilita',
+                textAlign: 'center',
+                fillStyle: '#ABC954',
+                coords: { x: 740, y: 1060 },
+            },
+            date: {
+                font: 'bold 40px Open Sans Condensed Light',
+                fillStyle: 'black',
+                textAlign: 'left',
+                coords: { x: 482, y: 1345 },
+            },
         },
-        date: {
-            font: 'bold 40px Open Sans Condensed Light',
-            fillStyle: 'black',
-            textAlign: 'left',
-            coords: { x: 482, y: 1345 },
+    },
+    'Tie Dye': {
+        filename: 'tie-dye.png',
+        textInfo: {
+            childName: {
+                font: '120px lilita',
+                textAlign: 'center',
+                fillStyle: '#ABC954',
+                coords: { x: 740, y: 1060 },
+            },
+            date: {
+                font: 'bold 40px Open Sans Condensed Light',
+                fillStyle: 'black',
+                textAlign: 'left',
+                coords: { x: 482, y: 1345 },
+            },
         },
     },
 }
