ba37d6dcb8a35e902c44c3b70156baf33973b2d1
Ryan Saffer <ryansaffer@gmail.com>
2024-11-11 10:47:25 +1100

    updated weekly scheduled function to return a Response

--- apps/website/netlify/functions/weekly-scheduled-build.ts
+++ apps/website/netlify/functions/weekly-scheduled-build.ts
@@ -3,13 +3,19 @@ import type { Config } from "@netlify/functions";
 // a scheduled function to trigger a build hook once a week.
 // a simple way to update instagram feed, google reviews etc.
 export default async () => {
-  await fetch("https://api.netlify.com/build_hooks/66e7cd2c0165a8c4b69fac25", {
-    method: "POST",
-  }).then((response) => {
+  try {
+    const response = await fetch(
+      "https://api.netlify.com/build_hooks/66e7cd2c0165a8c4b69fac25",
+      {
+        method: "POST",
+      },
+    );
     console.log("Build hook response:", response);
-  });
-
-  return { statusCode: 200 };
+    return new Response("ok");
+  } catch (err) {
+    console.error("Build hook error:", err);
+    return Response.error();
+  }
 };
 
 export const config: Config = {
