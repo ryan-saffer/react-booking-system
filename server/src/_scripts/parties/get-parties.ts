7650b1c085a232d1c554e3dbcbb6283bbcc0ee79
Ryan Saffer <ryansaffer@gmail.com>
2024-12-10 16:27:58 +1100

    end of year stats script

--- server/src/_scripts/parties/get-parties.ts
+++ server/src/_scripts/parties/get-parties.ts
@@ -1,4 +1,4 @@
-import { Booking, Location } from 'fizz-kidz'
+import { Booking, Creations, Location } from 'fizz-kidz'
 import { DateTime } from 'luxon'
 
 import { FirestoreClient } from '../../firebase/FirestoreClient'
@@ -46,5 +46,80 @@ export async function getParties({
               })
             : bookings
 
-    filteredBookings.forEach((booking) => console.log(booking))
+    // TO GET ALL PARTIES, JUST LOG `filteredBookings` NOW.
+    // THE FOLLOWING CODE WAS USED TO GET END OF YEAR STATS FOR 2024.
+
+    const total = filteredBookings.length
+    const balwyn = filteredBookings.filter((it) => it.location === Location.BALWYN && it.type === 'studio')
+    const cheltenam = filteredBookings.filter((it) => it.location === Location.CHELTENHAM && it.type === 'studio')
+    const essendon = filteredBookings.filter((it) => it.location === Location.ESSENDON && it.type === 'studio')
+    const malvern = filteredBookings.filter((it) => it.location === Location.MALVERN && it.type === 'studio')
+    const mobile = filteredBookings.filter((it) => it.type === 'mobile')
+
+    console.log('TOTAL BOOKINGS: ', total)
+    console.log('Balwyn: ', balwyn.length)
+    console.log('Cheltenham: ', cheltenam.length)
+    console.log('Essendon: ', essendon.length)
+    console.log('Malvern: ', malvern.length)
+    console.log('Mobile: ', mobile.length)
+
+    const creations: Partial<Record<Creations, number>> = {}
+
+    filteredBookings.forEach((booking) => {
+        if (booking.creation1) {
+            const creation = booking.creation1
+            if (creations[creation]) {
+                creations[creation] += 1
+            } else {
+                creations[booking.creation1] = 1
+            }
+        }
+        if (booking.creation2) {
+            const creation = booking.creation2
+            if (creations[creation]) {
+                creations[creation] += 1
+            } else {
+                creations[booking.creation2] = 1
+            }
+        }
+        if (booking.creation3) {
+            const creation = booking.creation3
+            if (creations[creation]) {
+                creations[creation] += 1
+            } else {
+                creations[booking.creation3] = 1
+            }
+        }
+    })
+
+    console.log({ creations })
+
+    const holSnap = await firestore.collectionGroup('programs').get()
+    const programs = holSnap.docs.filter((it) => {
+        const program = it.data()
+        if (
+            new Date(program.dateTime) > DateTime.fromObject({ year: 2024, month: 1, day: 1 }).toJSDate() &&
+            new Date(program.dateTime) < DateTime.fromObject({ year: 2024, month: 12, day: 31 }).toJSDate()
+        ) {
+            return true
+        }
+        return false
+    })
+
+    console.log('Number of holiday program bookings: ', programs.length)
+
+    const eventsSnap = await firestore.collectionGroup('eventSlots').get()
+    const eventIds: Record<string, boolean> = {}
+    let totalSlots = 0
+    eventsSnap.docs.forEach((doc) => {
+        const event = doc.data()
+        const startTime = event.startTime.toDate() as Date
+        if (startTime.getFullYear() === 2024) {
+            eventIds[event.eventId] = true
+            totalSlots++
+        }
+    })
+
+    console.log('Total events: ', Object.keys(eventIds).length)
+    console.log('Total event slots: ', totalSlots)
 }
