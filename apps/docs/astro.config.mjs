c49d48f90080f4981f1d77044f0f145b141182b9
Ryan Saffer <ryan.saffer@7eleven.com.au>
2025-09-24 14:20:17 +1000

    use catppuccin theme

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import catppuccin from "@catppuccin/starlight";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
         light: "./src/assets/logo-stacked.png",
         dark: "./src/assets/logo-white.png",
       },
+      plugins: [catppuccin()],
       sidebar: [
         {
           label: "Tools",
