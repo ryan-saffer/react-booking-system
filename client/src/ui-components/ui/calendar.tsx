bbfca263601e055cfa376ee0d450cf4553800b9b
Ryan Saffer <ryansaffer@gmail.com>
2026-01-06 15:55:40 +1100

    change calendar to use tailwind v3 features
    it was using tailwind v4 which isnt supported so it looked off

--- client/src/ui-components/ui/calendar.tsx
+++ client/src/ui-components/ui/calendar.tsx
@@ -24,7 +24,7 @@ function Calendar({
             fixedWeeks
             showOutsideDays={showOutsideDays}
             className={cn(
-                'group/calendar bg-background p-3 [--cell-size:--spacing(8)] [[data-slot=card-content]_&]:bg-transparent [[data-slot=popover-content]_&]:bg-transparent',
+                'group/calendar bg-background p-3 [--cell-size:2rem] [[data-slot=card-content]_&]:bg-transparent [[data-slot=popover-content]_&]:bg-transparent',
                 String.raw`rtl:**:[.rdp-button\_next>svg]:rotate-180`,
                 String.raw`rtl:**:[.rdp-button\_previous>svg]:rotate-180`,
                 className
@@ -44,20 +44,20 @@ function Calendar({
                 ),
                 button_previous: cn(
                     buttonVariants({ variant: buttonVariant }),
-                    'size-(--cell-size) aria-disabled:opacity-50 p-0 select-none',
+                    'h-[var(--cell-size)] w-[var(--cell-size)] aria-disabled:opacity-50 p-0 select-none',
                     defaultClassNames.button_previous
                 ),
                 button_next: cn(
                     buttonVariants({ variant: buttonVariant }),
-                    'size-(--cell-size) aria-disabled:opacity-50 p-0 select-none',
+                    'h-[var(--cell-size)] w-[var(--cell-size)] aria-disabled:opacity-50 p-0 select-none',
                     defaultClassNames.button_next
                 ),
                 month_caption: cn(
-                    'flex items-center justify-center h-(--cell-size) w-full px-(--cell-size)',
+                    'flex items-center justify-center h-[var(--cell-size)] w-full px-[var(--cell-size)]',
                     defaultClassNames.month_caption
                 ),
                 dropdowns: cn(
-                    'w-full flex items-center text-sm font-medium justify-center h-(--cell-size) gap-1.5',
+                    'w-full flex items-center text-sm font-medium justify-center h-[var(--cell-size)] gap-1.5',
                     defaultClassNames.dropdowns
                 ),
                 dropdown_root: cn(
@@ -79,7 +79,7 @@ function Calendar({
                     defaultClassNames.weekday
                 ),
                 week: cn('flex w-full mt-2', defaultClassNames.week),
-                week_number_header: cn('select-none w-(--cell-size)', defaultClassNames.week_number_header),
+                week_number_header: cn('select-none w-[var(--cell-size)]', defaultClassNames.week_number_header),
                 week_number: cn('text-[0.8rem] select-none text-muted-foreground', defaultClassNames.week_number),
                 day: cn(
                     'relative w-full h-full p-0 text-center [&:last-child[data-selected=true]_button]:rounded-r-md group/day aspect-square select-none',
@@ -119,7 +119,7 @@ function Calendar({
                 WeekNumber: ({ children, ...props }) => {
                     return (
                         <td {...props}>
-                            <div className="size-(--cell-size) flex items-center justify-center text-center">
+                            <div className="h-[var(--cell-size)] w-[var(--cell-size)] flex items-center justify-center text-center">
                                 {children}
                             </div>
                         </td>
@@ -153,7 +153,7 @@ function CalendarDayButton({ className, day, modifiers, ...props }: React.Compon
             data-range-end={modifiers.range_end}
             data-range-middle={modifiers.range_middle}
             className={cn(
-                'min-w-(--cell-size) flex aspect-square size-auto w-full flex-col gap-1 font-normal leading-none data-[range-end=true]:rounded-md data-[range-middle=true]:rounded-none data-[range-start=true]:rounded-md data-[range-end=true]:rounded-r-md data-[range-start=true]:rounded-l-md data-[range-end=true]:bg-primary data-[range-middle=true]:bg-accent data-[range-start=true]:bg-primary data-[selected-single=true]:bg-primary data-[range-end=true]:text-primary-foreground data-[range-middle=true]:text-accent-foreground data-[range-start=true]:text-primary-foreground data-[selected-single=true]:text-primary-foreground group-data-[focused=true]/day:relative group-data-[focused=true]/day:z-10 group-data-[focused=true]/day:border-ring group-data-[focused=true]/day:ring-[3px] group-data-[focused=true]/day:ring-ring/50 dark:hover:text-accent-foreground [&>span]:text-xs [&>span]:opacity-70',
+                'min-w-[var(--cell-size)] flex aspect-square h-auto w-full flex-col gap-1 font-normal leading-none data-[range-end=true]:rounded-md data-[range-middle=true]:rounded-none data-[range-start=true]:rounded-md data-[range-end=true]:rounded-r-md data-[range-start=true]:rounded-l-md data-[range-end=true]:bg-primary data-[range-middle=true]:bg-accent data-[range-start=true]:bg-primary data-[selected-single=true]:bg-primary data-[range-end=true]:text-primary-foreground data-[range-middle=true]:text-accent-foreground data-[range-start=true]:text-primary-foreground data-[selected-single=true]:text-primary-foreground group-data-[focused=true]/day:relative group-data-[focused=true]/day:z-10 group-data-[focused=true]/day:border-ring group-data-[focused=true]/day:ring-[3px] group-data-[focused=true]/day:ring-ring/50 dark:hover:text-accent-foreground [&>span]:text-xs [&>span]:opacity-70',
                 defaultClassNames.day,
                 className
             )}
